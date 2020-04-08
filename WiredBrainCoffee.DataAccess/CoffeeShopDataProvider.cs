54acccd0529350d1d202c540238ba5acfe4e9bed
Per Lindgren <per.lindgren@cerner.com>
2020-04-08 21:04:21 +0200

    Add PaperCupsInStock property

--- WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -3,13 +3,28 @@ using WiredBrainCoffee.DataAccess.Model;
 
 namespace WiredBrainCoffee.DataAccess
 {
-   public class CoffeeShopDataProvider
+    public class CoffeeShopDataProvider
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops()
         {
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
-            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+            yield return new CoffeeShop
+            {
+                Location = "Frankfurt",
+                BeansInStockInKg = 107,
+                PaperCupsInStock = 350
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Freiburg",
+                BeansInStockInKg = 23,
+                PaperCupsInStock = 250
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Munich",
+                BeansInStockInKg = 56,
+                PaperCupsInStock = 427
+            };
         }
     }
 }
