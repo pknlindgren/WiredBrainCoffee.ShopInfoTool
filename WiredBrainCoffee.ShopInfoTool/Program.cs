9f88977c29eb96c3efa9908f33ab13b5ecc2353e
Per Lindgren <per.lindgren@cerner.com>
2020-04-08 18:44:02 +0200

    Implement quit command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,14 +9,17 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Write 'help' to list available commands");
+            Console.WriteLine("Write 'help' to list available commands" + "write 'quit' to exit application");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
             while (true)
             {
                 var line = Console.ReadLine();
-
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                { 
+                    break;
+                }
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
                 {
